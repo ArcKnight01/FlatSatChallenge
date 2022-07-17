b3744cf4e7208a594d1b61f77513a4a223e06cd7
ranvitha <ranvitha@mit.edu>
2022-07-17 03:50:41 -0500

    New Photo

--- Images/ranvitha/FlatSat_student.py
+++ Images/ranvitha/FlatSat_student.py
@@ -11,25 +11,27 @@ import busio
 import adafruit_fxos8700
 from git import Repo
 from picamera import PiCamera
+from time import sleep
+import subprocess
 
 #setup imu and camera
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_fxos8700.FXOS8700(i2c)
-camera = PiCamera()
+#camera = PiCamera()
 
 #function for uploading image to Github
 def git_push():
 	try:
 		repo = Repo('/home/pi/FlatSatChallenge')
-        	repo.git.add('folder path') #PATH TO YOUR IMAGES FOLDER, SHOULD BE LOCATED IN FlatSatChallenge/Images/YOURFOLDER
-        	repo.index.commit('New Photo')
-        	print('made the commit')
-        	origin = repo.remote('origin')
-        	print('added remote')
-        	origin.push()
-        	print('pushed changes')
-    	except:
-        	print('Couldn\'t upload to git')
+		repo.git.add('/home/pi/FlatSatChallenge/Images/ranvitha') #PATH TO YOUR IMAGES FOLDER, SHOULD BE LOCATED IN FlatSatChallenge/Images/YOURFOLDER
+		repo.index.commit('New Photo')
+		print('made the commit')
+		origin = repo.remote('origin')
+		print('added remote')
+		origin.push()
+		print('pushed changes')
+	except:
+		print('Couldn\'t upload to git')
 
 
 #SET THRESHOLD
@@ -41,15 +43,22 @@ while True:
 	accelX, accelY, accelZ = sensor.accelerometer
     #CHECK IF READINGS ARE ABOVE THRESHOLD
         #PAUSE
-	if (accelx > thershold or accely > thershold or accelz > thershold):
+	if (accelX > threshold or accelY > threshold or accelZ > threshold):
 		time.sleep(1)
 
         #TAKE/SAVE/UPLOAD A PICTURE
 		name = "MuramreddyR"     #Last Name, First Initial  ex. FoxJ
-        	if name:
-            		t = time.strftime("_%H%M%S")      # current time string
-            		imgname = ('/home/pi/FlatSatChallenge/Images/ranvitha/%s%s' % (name,t)) #change directory to your folder
-	    		camer.capture(imgname)
+		if name:
+			t = time.strftime("_%H%M%S")      # current time string
+			#camera.start_preview()
+			#sleep(5)
+			#camera.stop_preview()
+			imgname = ('/home/pi/FlatSatChallenge/Images/ranvitha/%s%s' % (name,t)) #change directory to your folder
+			imgname = 'ranvitha.jpg'
+			#camera.capture(imgname + '.jpg')
+			cmd = "raspistill -vf -hf /home/pi/FlatSatChallenge/Images/ranvitha/" + imgname+ ".jpg"
+			subprocess.call(cmd, shell = True)
+			git_push()
             #<YOUR CODE GOES HERE>#
       #PAUSE
-	    		time.sleep(1)
+	time.sleep(1)
